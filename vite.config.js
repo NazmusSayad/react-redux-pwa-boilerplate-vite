6052593aa53fb0de131c615fc3e12a08e9c0189b
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2022-10-18 12:44:40 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,16 @@
+import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from '@honkhonk/vite-plugin-svgr'
 import { VitePWA } from 'vite-plugin-pwa'
 const isDevMode = process.env.NODE_ENV !== 'production'
-
 const config = {
   static: 'static',
   assets: 'assets'
 }
 
 const server = {
+  host: 'localhost',
   port: 3000,
 }
 
@@ -24,6 +25,20 @@ const build = {
       entryFileNames: `${config.static}/[name]-[hash].js`,
       chunkFileNames: `${config.static}/chunk-[name]-[hash].js`,
     },
+  },
+}
+
+const resolve = {
+  alias: {
+      '@src': path.resolve('./src'),
+      '@hooks': path.resolve('./src/hooks'),
+      '@store': path.resolve('./src/store'),
+      '@slice': path.resolve('./src/store/slice'),
+      '@assets': path.resolve('./src/assets'),
+      '@components': path.resolve('./src/components'),
+      '@layouts': path.resolve('./src/layouts'),
+      '@pages': path.resolve('./src/pages'),
+      '@abstracts': path.resolve('./src/styles/abstracts'),
   },
 }
 
